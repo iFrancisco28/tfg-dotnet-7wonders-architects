cbf48ebb16fb210a44b2ac0d346540aa91928259
Francisco Carrero Carrero <fracarrero05@gmail.com>
2026-03-13 01:08:50 +0100

    diseño en loginpage

--- TFG_FranciscoCarreroCarrero_7WondersArchitects/MauiProgram.cs
+++ TFG_FranciscoCarreroCarrero_7WondersArchitects/MauiProgram.cs
@@ -1,25 +1,45 @@
 ﻿using Microsoft.Extensions.Logging;
 using CommunityToolkit.Maui;
 
-namespace TFG_FranciscoCarreroCarrero_7WondersArchitects
-{
-    public static class MauiProgram
-    {
-        public static MauiApp CreateMauiApp()
-        {
+namespace TFG_FranciscoCarreroCarrero_7WondersArchitects {
+    public static class MauiProgram {
+        public static MauiApp CreateMauiApp() {
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
                 .UseMauiCommunityToolkit()
-                .ConfigureFonts(fonts =>
-                {
+                .ConfigureFonts(fonts => {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
+
+            //para quitar las lineas transaparentes de los entry y los picker 
+
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping("NoUnderline", (h, v) => {
+#if ANDROID
+                h.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+#elif IOS || MACCATALYST
+                h.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                h.PlatformView.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
+#endif
+            });
+
+            Microsoft.Maui.Handlers.PickerHandler.Mapper.AppendToMapping("NoUnderline", (h, v) => {
+#if ANDROID
+                h.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+#elif IOS || MACCATALYST
+                h.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#elif WINDOWS
+                h.PlatformView.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
+#endif
+            });
+
+            // 
 
             return builder.Build();
         }
